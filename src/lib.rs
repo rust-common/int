e8bce02d8a60ef1a3bf8b72ba826f12c67ac9c24
Sergey Shandar <sergey.shandar@gmail.com>
2018-11-17 18:01:20 -0800

    no dependency on u8 constrains

--- src/lib.rs
+++ src/lib.rs
@@ -8,30 +8,35 @@ pub trait UInt where
         std::ops::SubAssign<Self> +
         std::ops::AddAssign +
         std::ops::Shl<u8, Output = Self> +
-        std::ops::ShrAssign<u8>,
-    u8: AsPrimitive<Self>
+        std::ops::ShrAssign<u8>
 {
     const _0: Self;
     const _1: Self;
+    fn from_u8(v: u8) -> Self;
 }
 
 impl UInt for u8 {
     const _0: u8 = 0;
     const _1: u8 = 1;
+    fn from_u8(v: u8) -> Self { v }
 }
 impl UInt for u16 {
     const _0: u16 = 0;
     const _1: u16 = 1;
+    fn from_u8(v: u8) -> Self { v as Self }
 }
 impl UInt for u32 {
     const _0: u32 = 0;
     const _1: u32 = 1;
+    fn from_u8(v: u8) -> Self { v as Self }
 }
 impl UInt for u64 {
     const _0: u64 = 0;
     const _1: u64 = 1;
+    fn from_u8(v: u8) -> Self { v as Self }
 }
 impl UInt for u128 {
     const _0: u128 = 0;
     const _1: u128 = 1;
+    fn from_u8(v: u8) -> Self { v as Self }
 }
