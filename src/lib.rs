fbdae2ba80663c59c0fd6f27e65e28c1029b9bd7
Sergey Shandar <sergey.shandar@gmail.com>
2018-11-17 19:43:43 -0800

    Shr

--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,7 @@ pub trait UInt where
         std::ops::AddAssign +
         std::ops::DivAssign +
         std::ops::Shl<u8, Output = Self> +
+        std::ops::Shr<u8, Output = Self> +
         std::ops::ShrAssign<u8> +
         std::ops::Sub<Output = Self> +
         std::ops::SubAssign<Self>
